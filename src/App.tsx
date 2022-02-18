3b3a71ff28c914bd0a3d3db4942739a318e357b1
minhnc <minhnc62@wru.vn>
2022-02-18 17:35:33 +0700

    component

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,34 @@
-import { useDispatch, useSelector } from "react-redux";
+
 import "./App.css";
-import { useTodo, add, edit, remove, clear } from "./store/todoSlide";
-import { useState } from "react";
+import { selectTodo, add, edit, remove, clear } from "./store/todoSlice";
+import { useState, useRef } from "react";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { Row, Col, Input, Button, Form, List } from "antd";
-import { DeleteOutlined, EditOutlined, CloseOutlined } from "@ant-design/icons";
 import styled from "styled-components";
+import { useEffect } from "react";
+import ListTodo from "./component/ListTodo";
+import FormInput from "./component/FormInput";
 
-// import Form from "./component/Form";
-// import List from "./component/List";
 
 const App = styled(Col)`
   text-align: center;
 `;
-const ListItem = styled(List.Item)`
-  text-align: unset;
-  display: flex;
-  justify-content: space-around;
-`;
-const IconsWrapper = styled.div`
-  display: flex;
-  column-gap: 8px;
-`;
+// const ListItem = styled(List.Item)`
+//   text-align: unset;
+//   display: flex;
+//   justify-content: space-around;
+// `;
+
 
 export default () => {
-  const dispatch = useAppDispatch();
-  const todos: any = useAppSelector(useTodo);
-  const [editing, setEditing] = useState(null);
-  const [todo, setTodo] = useState("");
-
-  const handleSubmit = (e: any) => {
-    if (todo.trim().length == 0) return;
-
-    if (editing) {
-      dispatch(edit({ id: editing, content: todo }));
-      setEditing(null);
-    } else {
-      dispatch(add(todo));
-    }
-
-    setTodo(" ");
-    console.log("oke");
-  };
+  const dispatch = useAppDispatch()
 
   return (
     <Row justify="center">
       <App span={6}>
         <h1 className="header"> Todo list</h1>
-        <Form onFinish={handleSubmit}>
-          <Input
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
-            placeholder="Type something"
-            style={{ width: "calc(100% - 200px)", marginRight: "4px" }}
-            defaultValue="https://ant.design"
-          />
-          {editing ? (
-            <>
-              <Button onClick={handleSubmit}  type="primary">Edit</Button>
-              <Button
-                type="ghost"
-                onClick={() => {
-                  setEditing(null);
-                  setTodo("");
-                }}
-              >
-                <CloseOutlined />
-              </Button>
-            </>
-          ) : (
-            <Button onClick={handleSubmit} type="primary">
-              Add
-            </Button>
-          )}
-        </Form>
-        <List className="todo-list">
-          {todos.map((g: any) => (
-            <ListItem key={g.id}>
-              {g.content}
-              <IconsWrapper>
-                <DeleteOutlined onClick={() => dispatch(remove(g.id))} />
-                <EditOutlined
-                  onClick={() => {
-                    setEditing(g.id);
-                    setTodo(g.content);
-                  }}
-                />
-              </IconsWrapper>
-            </ListItem>
-          ))}
-        </List>
+        <FormInput/>
+        <ListTodo/>
         <Button
           style={{ marginTop: "20px" }}
           type="primary"
---
+++ src/component/ListItem.tsx
@@ -0,0 +1,73 @@
+import { Button } from "antd";
+import { DeleteOutlined, EditOutlined, CloseOutlined } from "@ant-design/icons";
+import styled from "styled-components";
+import { edit, remove } from "../store/todoSlice";
+import { useState, useRef } from "react";
+import { useAppDispatch } from "../store/hooks";
+import { useEffect } from "react";
+import { todoState } from "../store/todoSlice";
+
+interface TodoItemProps {
+    item: todoState;
+  editing: any;
+  
+}
+
+export default ({ item: { id, content },}: TodoItemProps):JSX.Element=> {
+  const dispatch = useAppDispatch();
+  const [editing, setEditing] = useState<TodoItemProps["editing"]>(null);
+  const [todoEdit, setTodoEdit] = useState("");
+
+  const inputText = useRef<HTMLInputElement>(null);
+  useEffect(() => inputText?.current?.focus());
+
+  const handleInput = (e: any) => {
+    if (editing) {
+      dispatch(edit({ id: editing, content: todoEdit }));
+      setEditing(null);
+    }
+  };
+
+  const IconsWrapper = styled.div`
+    display: flex;
+    column-gap: 8px;
+  `;
+  return (
+    <div className="list">
+      <li className="list-item">
+        {!editing ? (
+          <>
+            {content}
+            <IconsWrapper>
+              <DeleteOutlined onClick={() => dispatch(remove(id))} />
+              <EditOutlined
+                onClick={() => {
+                  setEditing(id);
+                }}
+              />
+            </IconsWrapper>
+          </>
+        ) : (
+          <>
+            <input
+              className="edit-item"
+              ref={inputText}
+              defaultValue={content}
+              onChange={(e) => setTodoEdit(e.target.value)}
+            />
+            <Button onClick={handleInput} type="primary">
+              Edit
+            </Button>
+            <Button
+              type="ghost"
+              onClick={() => {
+                setEditing(null);
+                setTodoEdit(content);
+              }}
+            ></Button>
+          </>
+        )}
+      </li>
+    </div>
+  );
+};
