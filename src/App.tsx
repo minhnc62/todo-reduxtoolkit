c88fcee6b0b1df1d783649424260686898e6ca18
Trương Công Nam <tgcgnam@Nam-Mac-Pro.local>
2022-02-18 14:29:34 +0700

    testpush

--- src/App.tsx
+++ src/App.tsx
@@ -141,6 +141,14 @@ export default () => {
         >
           Clear All
         </Button>
+        <Button
+          style={{ marginTop: '20px' }}
+          type="primary"
+          className="btn-clear"
+          onClick={() => dispatch(clear())}
+        >
+          Clear All
+        </Button>
       </App>
     </RowStyled>
   );
